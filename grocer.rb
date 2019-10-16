e69ec25492093cde383caaf0c706550fc1278e43
Emily Haynes <ehayne00@hotmail.com>
2019-10-16 12:58:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,45 @@
 def consolidate_cart(cart)
-  # code here
+receipt = {}
+cart.each { |item_hash| 
+  item_name = item_hash.keys[0]
+  
+  if receipt.has_key?(item_name)
+    receipt[item_name][:count] +=1
+    
+  else
+    receipt[item_name] = {
+      count: 1,
+      price: item_hash[item_name][:price],
+      clearance: item_hash[item_name][:clearance]
+    }
+end
+}
+receipt
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+ coupons.each do |coupon|
+ item = coupon[:item]
+ if cart[item]
+   if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
+     cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
+     cart[item][:count] -= coupon[:num]
+    elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
+     cart["#{item} W/COUPON"][:count] += coupon[:num]
+     cart[item][:count] -= coupon[:num]
+   end
+  end
+end
+     cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, stats|
+    stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
+  end 
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  
 end
