947dbe16cbea7172d9da19df77abf734b3a3d51f
Emily Haynes <ehayne00@hotmail.com>
2019-10-16 13:13:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,6 +44,6 @@ def checkout(cart, coupons)
   hash_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
-  total = applied_discount.reduce(0) {|acc, (key, value)| acc += }
+  total = applied_discount.reduce(0) {|acc, (key, value)| acc += 
   total > 100 ? total * 0.9 : total
 end
